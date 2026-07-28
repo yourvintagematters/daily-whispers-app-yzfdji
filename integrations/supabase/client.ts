1823597ae41c1ddb6d5a9c431a415bfd93aef274
newly.dev <api@newly.dev>
2026-07-28 01:49:59 +0000

    fix: Point app to correct Supabase project

--- integrations/supabase/client.ts
+++ integrations/supabase/client.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { Database } from './types';
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://rfaqhkbbyvddaxyolxrx.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmYXFoa2JieXZkZGF4eW9seHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwOTQyNTgsImV4cCI6MjA3ODY3MDI1OH0.A13u_utRZSifaN4-tUBXJ-C57qNMpt70BGa8YxRF3_E";
+const SUPABASE_URL = "https://cyktcpdmlsfjyrnutmln.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5a3RjcGRtbHNmanlybnV0bWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NzY4NTksImV4cCI6MjA5NDU1Mjg1OX0.iHgBFydklpqRZxRBy0yHlnw3CVTlh9Npaua1bzqQV-s";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
