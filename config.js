9f13baa9823df78dddd816ab35a21b06ffa33ba8
Derryth Nash Creative <yourvintagematters@gmail.com>
2026-08-21 10:15:00 +1000

    Create config.js with environment constants

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+
+export const MODE = "production";
+
+export const SUPABASE_URL = "";
+export const SUPABASE_KEY = "";
+
+export const STRIPE_PUBLIC_KEY = "";
+
+export const IS_TEST_MODE = MODE === "test";
